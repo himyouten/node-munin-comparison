ca9c40ddd5c49ea998fc8b9260d6ca826a52d758
Him You Ten <himyouten@gmail.com>
2014-07-08 17:17:49 -0400

    removed un needed paritals-other view dir

--- config/express-hbs.js
+++ config/express-hbs.js
@@ -9,7 +9,7 @@ app.use("/public", express.static(rootdir + '/public'));
 // Hook in express-hbs and tell it where known directories reside
 var viewsDir = rootdir + '/views';
 app.engine('hbs', hbs.express3({
-    partialsDir: [rootdir + '/views/partials', rootdir + '/views/partials-other'],
+    partialsDir: [rootdir + '/views/partials'],
     defaultLayout: rootdir + '/views/layout/default.hbs'
 }));
 app.set('view engine', 'hbs');
